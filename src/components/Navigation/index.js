0a21ea0ecb3a260fb4e4102377c0964c35d1662e
pmasharov <p.n.masharov@gmail.com>
2022-12-20 21:04:53 +0300

    navigation styles moved to separate module; add favorites list to favorites page;

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,17 +1,28 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { FavoritesContext } from "store/favorites";
+import styles from "./styles.module.scss";
 
 const Navigation = () => {
+	const favoritesCtx = useContext(FavoritesContext);
+
 	return (
-		<nav>
+		<nav className={styles.navigation}>
 			<ul>
 				<li>
 					<Link to={"/"}>All meetups</Link>
 				</li>
 				<li>
-					<Link to={"/favorites"}>Favorites</Link>
+					<Link to={"new-meetup"}>Add new meetup</Link>
 				</li>
 				<li>
-					<Link to={"new-meetup"}>Add new meetup</Link>
+					<Link
+						to={"favorites"}
+						className={styles.withBadge}
+						data-count={favoritesCtx.totalFavorites}
+					>
+						Favorites
+					</Link>
 				</li>
 			</ul>
 		</nav>
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,23 @@
+import { MeetupList } from "components";
+import { useContext } from "react";
+import { FavoritesContext } from "store/favorites";
+
 const FavoritesPage = () => {
-	return <div>Favorites page</div>;
+	const favoritesCtx = useContext(FavoritesContext);
+
+	const content =
+		favoritesCtx.totalFavorites === 0 ? (
+			<p>You got no favorite meetups yet</p>
+		) : (
+			<MeetupList meetups={favoritesCtx.favorites} />
+		);
+
+	return (
+		<section>
+			<h1>Favorites</h1>
+			{content}
+		</section>
+	);
 };
 
 export { FavoritesPage };
